483b158c7904ecad61d516cc8011f4707edbd673
laurafernandez1152 <laurafernandez0907@gmail.com>
2023-10-06 20:51:39 -0500

    cambio2

--- pages/3_Aplicación 2.py
+++ pages/3_Aplicación 2.py
@@ -1,22 +1,22 @@
 import streamlit as st
 import matplotlib.pyplot as plt
+import pandas as pd
 
 def main():
     # Datos ficticios: Categorías y valores
     categorias = ['Categoría A', 'Categoría B', 'Categoría C', 'Categoría D']
     valores = [15, 24, 30, 8]
     
-    # Crear un gráfico de barras
-    fig, ax = plt.subplots()
-    ax.bar(categorias, valores, color='skyblue')
+    # Crear un DataFrame para facilitar el uso con st.bar_chart
+    df = pd.DataFrame({'Categorías': categorias, 'Valores': valores})
     
-    # Personalizar el gráfico
-    ax.set_title('Gráfico de Barras')
-    ax.set_xlabel('Categorías')
-    ax.set_ylabel('Valores')
-    
-    # Mostrar el gráfico en Streamlit
-    st.pyplot(fig)
+    # Mostrar el gráfico de barras en Streamlit
+    st.bar_chart(df.set_index('Categorías'))
 
+    # Personalizar el gráfico (esto no es necesario en Streamlit)
+    plt.title('Gráfico de Barras')
+    plt.xlabel('Categorías')
+    plt.ylabel('Valores')
+    
 if __name__ == "__main__":
     main()
